919dcb0994f015b2d221ab5d7be1ed4661a885c3
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2023-12-04 15:48:41 +0300

    Added solution for Verifying an Alien Dictionary

---
+++ week-1/verifying-an-alien-dictionary.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_map = {}
+        for i, v in enumerate(order):
+            order_map[v] = i
+
+        def cmp_alien(x, y):
+            ls = min(len(x), len(y))
+            index = 0
+            while index < ls:
+                if x[index] != y[index]:
+                    return order_map[x[index]] - order_map[y[index]]
+                index += 1
+            return len(x) - len(y)
+        pos = 0
+        while pos + 1 < len(words):
+            if cmp_alien(words[pos], words[pos + 1]) > 0:
+                return False
+            pos += 1
+        return True

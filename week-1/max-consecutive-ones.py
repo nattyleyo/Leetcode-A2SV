bb858b3393dc1b184b2683f2cc6cb06779117fb7
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2023-12-04 16:34:42 +0300

    Added solution for Max Consecutive Ones

---
+++ week-1/max-consecutive-ones.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        ans=count=0
+        for i in range(len(nums)):
+            if nums[i]==1:
+                    count+=1
+                    ans=max(ans,count)
+            else:
+                count = 0
+        return ans

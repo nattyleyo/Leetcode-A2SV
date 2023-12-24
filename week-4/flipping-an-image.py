241e0e5a6d18ce60808cd36ac83e2b707501c92f
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2023-12-24 21:01:12 +0300

    Added solution for Flipping an Image

---
+++ week-4/flipping-an-image.py
@@ -0,0 +1,12 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        n = len(image)
+        for i in range(n):
+            for j in range(n):
+                if image[i][j] == 0:
+                    image[i][j] = 1
+                else:
+                    image[i][j] = 0
+            image[i] = (list(reversed(image[i])))
+        return image
+        

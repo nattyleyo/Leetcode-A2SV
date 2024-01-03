1ca891414dfd24b50adcb5b85fa7ecb43aa52138
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2024-01-03 18:52:20 +0300

    Added solution for Remove Duplicates from Sorted Array

--- week-5/remove-duplicates-from-sorted-array.py
+++ week-5/remove-duplicates-from-sorted-array.py
@@ -5,11 +5,13 @@ class Solution:
         right=1
         dup=0
         k=1
+        count=0
         while right<n:
             if left ==right:
                 right+=1
             elif nums[left]==nums[right]:
                 nums[right]='_'
+                count+=1
                 right+=1
             elif nums[left]=='_':
                 nums[left],nums[right]=nums[right],nums[left]
@@ -18,7 +20,7 @@ class Solution:
                 right+=1
             else:
                 left+=1
-            k= n- nums.count("_")
+            k= n- count
         return k
-#0  1   2   3   _   _  _   _   3   4
-#           ^              ^ 
+#0  1   1   1   2   2   3   3   4
+#^  ^ 

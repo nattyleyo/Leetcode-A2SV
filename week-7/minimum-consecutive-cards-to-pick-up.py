f0a533efe6a0626a8628d113bc1781684f86f0e4
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2024-01-12 19:01:20 +0300

    Added solution for Minimum Consecutive Cards to Pick Up

--- week-7/minimum-consecutive-cards-to-pick-up.py
+++ week-7/minimum-consecutive-cards-to-pick-up.py
@@ -5,7 +5,11 @@ class Solution:
         for right in range(len(cards)):
             if cards[right] in dic:
                 minlen=min(minlen, right-dic[cards[right]]+1)
-            dic[cards[right]]=right 
+            dic[cards[right]]=right
+        print(dic)
         if len(set(cards))==len(cards):
             return -1
         return minlen
+#{3}
+#3 4 2 3 4 7
+#^

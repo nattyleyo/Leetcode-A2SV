7aff9850cdd489ac782bdda1e15336bb2aede876
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2024-01-24 18:01:10 +0300

    Added solution for Linked List Cycle

--- week-8/linked-list-cycle.py
+++ week-8/linked-list-cycle.py
@@ -6,14 +6,22 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # slow=0
-        # fast=0
-        my_set=set()
-        current=head
-        while current:
-            my_set.add(current)
-            current=current.next
-            if current in my_set:
+        #approch-1 using set
+        # my_set=set()
+        # current=head
+        # while current:
+        #     my_set.add(current)
+        #     current=current.next
+        #     if current in my_set:
+        #         return True
+        # return False
+        
+        #approch-2 using pointer
+        slow=head
+        fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
                 return True
         return False
-        

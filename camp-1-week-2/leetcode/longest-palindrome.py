6bdd5792eaa9135bce2b6ff3f58528d223734e14
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2024-02-26 22:20:44 +0300

    Add solution for Longest Palindrome

---
+++ camp-1-week-2/leetcode/longest-palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        max_len = 0
+        count = Counter(s)
+        arr = [val for val in count.values()]
+        arr.sort(reverse=True)
+        if len(arr) == 1:
+            return arr[0]
+        print(arr)
+        odd = False
+        for i in range(len(arr)):
+            if arr[i]%2 != 0 and not odd :
+                max_len += arr[i]
+                odd = True      
+            elif arr[i]%2 != 0 and odd:
+                max_len += arr[i]-1
+            else:
+                max_len += arr[i]
+        return max_len
+#abccccdd
+#{a:1,b:1,c:4,d:2}
+#1,1,2,4
+#len=4+2

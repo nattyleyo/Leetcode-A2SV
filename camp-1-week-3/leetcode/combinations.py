48dcf87f1182aae6300c42dc0f25069cbb0ae25f
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2024-03-04 14:28:56 +0300

    Add solution for Combinations

--- camp-1-week-3/leetcode/combinations.py
+++ camp-1-week-3/leetcode/combinations.py
@@ -1,13 +1,20 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        nums = [i for i in range(1,n+1)]
         res = []
-        def backtrack(num,path):
+        def backtrack(i,path):
             if len(path)==k:
                 res.append(path[:])
                 return
-            for num in range(num,n+1):
-                path.append(num)
-                backtrack(num+1,path)
-                path.pop()
-        backtrack(1,[])
+            if i>= n:
+                return
+            #insert
+            path.append(nums[i])
+            backtrack(i+1,path)
+            path.pop()
+            
+            #no insert
+            backtrack(i+1,path)
+
+        backtrack(0,[])
         return res

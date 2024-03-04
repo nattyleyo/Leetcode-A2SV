8b959088d91f31c45f3e18f496ff23d9beba7abc
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2024-03-04 19:21:35 +0300

    Add solution for Permutations

---
+++ camp-1-week-3/leetcode/permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def backtrack(path):
+            if len(path)==len(nums):
+                res.append(path[:])
+                return 
+            for i in range(len(nums)):
+                if nums[i] in path:
+                    continue
+                path.append(nums[i])
+                backtrack(path)
+                path.pop()
+        backtrack([])
+        return res

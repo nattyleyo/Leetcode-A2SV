60244e35d447cd0abc50c2f3b406f43b9f9c9691
Natnael Meseret <117472732+nattyleyo@users.noreply.github.com>
2024-03-08 18:02:02 +0300

    Add solution for H-Index II

--- camp-1-week-3/leetcode/h-index-ii.py
+++ camp-1-week-3/leetcode/h-index-ii.py
@@ -5,8 +5,9 @@ class Solution:
         high = n - 1
         while low <= high:
             mid = (low + high)//2
-            if n-mid > citations[mid]:
-                low = mid + 1
-            else:
+
+            if citations[mid] >= n - mid:
                 high = mid - 1
+            else:
+                low = mid + 1
         return n - low
